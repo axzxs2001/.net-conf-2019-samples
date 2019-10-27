b84277ac697ad5cb36cfb4d58eb6f6e1da59ae2f
桂素伟 <axzxs2001@163.com>
2019-10-27 10:05:31 +0900

    demo01项目

---
+++ dotnetconfsamples/demo01/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace demo01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        config.AddJsonFile("configuration/appsettings.json", optional: true, reloadOnChange: true);
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
